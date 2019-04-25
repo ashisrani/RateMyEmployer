1b5edf69dbf15f3eb372d05bb86d8be07ee45fa7
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-04-25 16:11:02 -0700

    browser router addition

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import AmazonReview from './AmazonReview'
 class App extends Component {
   render() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className="App">
             <Nav/>
             <Switch>
