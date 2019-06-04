47a2876feae68ec9dd8425390d87dd55e8adb5e5
Chaitanya Gangavarapu <chaitanya.gangavarapu@gmail.com>
2019-06-04 15:18:15 -0700

    dropdown menu change

--- src/demo.js
+++ src/demo.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import AmazonReview from './AmazonReview';
+import { ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
 
 class Demo extends Component {
     render() {
         return (
-           <Dropdown></Dropdown>
+           <Drop></Drop>
         )
     }   
 }
 
-class Dropdown extends Component {
+class Drop extends Component {
     state = {
       isOpen: false
     };
@@ -18,29 +19,38 @@ class Dropdown extends Component {
     toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
   
     render() {
-      const menuClass = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
+      // const menuClass = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
       return (
-        <div className="dropdown" onClick={this.toggleOpen}>
-          <button
-            className="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-          >
-            Company?
-          </button>
-          <div className={menuClass} aria-labelledby="dropdownMenuButton">
-            <a className="dropdown-item" href="MicrosoftReview">
-              Microsoft
-            </a>
-            <a className="dropdown-item" href="AmazonReview">
-              Amazon
-            </a>
-            <a className="dropdown-item" href="DeloitteReview">
-              Deloitte
-            </a>
-          </div>
+        // <div className="dropdown" onClick={this.toggleOpen}>
+        //   <button
+        //     className="btn btn-secondary dropdown-toggle"
+        //     type="button"
+        //     id="dropdownMenuButton"
+        //     data-toggle="dropdown"
+        //     aria-haspopup="true"
+        //   >
+        //     Company?
+        //   </button>
+        //   <div className={menuClass} aria-labelledby="dropdownMenuButton">
+        //     <a className="dropdown-item" href="MicrosoftReview">
+        //       Microsoft
+        //     </a>
+        //     <a className="dropdown-item" href="AmazonReview">
+        //       Amazon
+        //     </a>
+        //     <a className="dropdown-item" href="DeloitteReview">
+        //       Deloitte
+        //     </a>
+        //   </div>
+        // </div>
+        <div>
+          <ButtonGroup> 
+            <DropdownButton as={ButtonGroup} title="Companies" id="loanlink">
+              <Dropdown.Item href="/AmazonReview">Amazon</Dropdown.Item>
+              <Dropdown.Item href="/MicrosoftReview">Microsoft</Dropdown.Item>
+              <Dropdown.Item href="/DeloitteReview">Deloitte</Dropdown.Item>
+            </DropdownButton>
+          </ButtonGroup>
         </div>
       );
     }
